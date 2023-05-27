d5e1ade3967a86aca8299ae9c2af91685a9aeeda
kichikuou <KichikuouChrome@gmail.com>
2023-05-27 15:42:16 +0900

    Launcher: Display icons in game list
    Use a file with .ico extension in the game directory.

--- project/app/src/main/java/io/github/kichikuou/xsystem4/LauncherActivity.kt
+++ project/app/src/main/java/io/github/kichikuou/xsystem4/LauncherActivity.kt
@@ -2,26 +2,116 @@ package io.github.kichikuou.xsystem4
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.media.MediaScannerConnection
 import android.os.Bundle
 import android.os.Environment
 import android.system.Os
 import android.text.Html
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ArrayAdapter
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
-import android.widget.Toast
 import java.io.File
 import java.nio.charset.Charset
 
-class LauncherActivity : Activity() {
-    data class Entry(val name: String, val path: File, val homedir: File, val hasError: Boolean)
-    private val games: ArrayList<Entry> = arrayListOf()
+private data class Item(val name: String, val path: File, val homedir: File, val icon: File?, val error: String?) {
+    companion object {
+        fun fromDirectory(dir: File, homedir: File, context: Context): Item {
+            var ini = File(dir, "System40.ini")
+            if (!ini.exists())
+                ini = File(dir, "AliceStart.ini")
+            if (!ini.exists()) {
+                val err = context.getString(R.string.toast_no_ini, dir.path)
+                Log.w("Launcher", err)
+                return Item(dir.name, dir, homedir, null, err)
+            }
+            val icon = findIcon(dir)
+            val regex = Regex("""GameName\s*=\s*"(.*)"""")
+            for (line in ini.readLines(Charset.forName("Shift_JIS"))) {
+                regex.matchEntire(line)?.let {
+                    return Item(it.groupValues[1], dir, homedir, icon, null)
+                }
+            }
+            val err = context.getString(R.string.toast_no_GameName, ini.path)
+            Log.w("Launcher", err)
+            return Item(dir.name, dir, homedir, icon, err)
+        }
 
+        private fun findIcon(dir: File): File? {
+            dir.listFiles()?.forEach {
+                if (it.extension == "ico") { return it }
+            }
+            return null
+        }
+    }
+}
+
+private class GameListAdapter(activity: Activity) : BaseAdapter() {
+    private val items: ArrayList<Item> = arrayListOf()
+    private val context: Context = activity
+
+    init {
+        for (storagePath in activity.getExternalFilesDirs(null)) {
+            val state = Environment.getExternalStorageState(storagePath)
+            Log.i("launcher", "${storagePath}: $state")
+            if (state != Environment.MEDIA_MOUNTED && state != Environment.MEDIA_MOUNTED_READ_ONLY) {
+                continue
+            }
+            val homedir = File(storagePath, ".xsystem4")
+            val files = storagePath.listFiles() ?: continue
+            for (path in files) {
+                if (!path.equals(homedir) && path.isDirectory) {
+                    items.add(Item.fromDirectory(path, homedir, activity))
+                }
+            }
+            if (state == Environment.MEDIA_MOUNTED && files.isEmpty()) {
+                // Create a dummy file and do a media scan so that `storagePath` is discoverable via MTP.
+                // https://issuetracker.google.com/issues/37071807
+                val dummyFile = File(storagePath, "dummy.txt")
+                dummyFile.writeText("Create a subfolder here that stores game files.")
+                MediaScannerConnection.scanFile(activity, arrayOf(dummyFile.absolutePath), null, null)
+            }
+            // Make sure that save directories are group-readable (so that they can be transferred
+            // via MTP), while old xsystem4 created them with permission 0700.
+            // TODO: Remove this after some transition period.
+            if (homedir.exists()) {
+                makeGroupReadable(homedir)
+            }
+        }
+    }
+
+    override fun getCount(): Int = items.size
+    override fun getItem(position: Int): Any = items[position]
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val item = items[position]
+        val view = convertView
+            ?: LayoutInflater.from(context).inflate(R.layout.launcher_item, parent, false)
+        if (item.icon != null) {
+            val bmp = BitmapFactory.decodeFile(item.icon.path)
+            view.findViewById<ImageView>(R.id.icon).setImageBitmap(bmp)
+        }
+        val title = view.findViewById<TextView>(R.id.title)
+        title.text = item.name
+        if (item.error != null) {
+            title.setTextColor(Color.GRAY)
+        }
+        return view
+    }
+}
+
+class LauncherActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.launcher)
@@ -30,9 +120,8 @@ class LauncherActivity : Activity() {
         listView.emptyView = findViewById(R.id.empty)
         findViewById<TextView>(R.id.usage).text = Html.fromHtml(getString(R.string.usage))
         listView.setOnItemClickListener { _, _, pos, _ ->
-            launchGame(games[pos])
+            launchGame(listView.adapter.getItem(pos) as Item)
         }
-
         updateGameList()
     }
 
@@ -63,77 +152,23 @@ class LauncherActivity : Activity() {
     }
 
     private fun updateGameList() {
-        games.clear()
-        for (storagePath in getExternalFilesDirs(null)) {
-            val state = Environment.getExternalStorageState(storagePath)
-            Log.i("launcher", "${storagePath}: $state")
-            if (state != Environment.MEDIA_MOUNTED && state != Environment.MEDIA_MOUNTED_READ_ONLY) {
-                continue
-            }
-            val homedir = File(storagePath, ".xsystem4")
-            val files = storagePath.listFiles() ?: continue
-            for (path in files) {
-                if (!path.equals(homedir) && path.isDirectory) {
-                    val title = getGameTitle(path)
-                    if (title != null) {
-                        games.add(Entry(title, path, homedir, false))
-                    } else {
-                        games.add(Entry(path.name + '?', path, homedir, true))
-                    }
-                }
-            }
-            if (state == Environment.MEDIA_MOUNTED && files.isEmpty()) {
-                // Create a dummy file and do a media scan so that `storagePath` is discoverable via MTP.
-                // https://issuetracker.google.com/issues/37071807
-                val dummyFile = File(storagePath, "dummy.txt")
-                dummyFile.writeText("Create a subfolder here that stores game files.")
-                MediaScannerConnection.scanFile(this, arrayOf(dummyFile.absolutePath), null, null)
-            }
-            // Make sure that save directories are group-readable (so that they can be transferred
-            // via MTP), while old xsystem4 created them with permission 0700.
-            // TODO: Remove this after some transition period.
-            if (homedir.exists()) {
-                makeGroupReadable(homedir)
-            }
-        }
-        val items = games.map(Entry::name).toMutableList()
         val listView = findViewById<ListView>(R.id.list)
-        listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        listView.adapter = GameListAdapter(this)
     }
 
-    private fun getGameTitle(dir: File): String? {
-        var ini = File(dir, "System40.ini")
-        if (!ini.exists())
-            ini = File(dir, "AliceStart.ini")
-        if (!ini.exists()) {
-            Log.w("Launcher", "Neither System40.ini nor AliceStart.ini in ${dir.path}")
-            return null
-        }
-        val regex = Regex("""GameName\s*=\s*"(.*)"""")
-        for (line in ini.readLines(Charset.forName("Shift_JIS"))) {
-            regex.matchEntire(line)?.let {
-                return it.groupValues[1]
-            }
-        }
-        val err = getString(R.string.toast_no_GameName, ini.path)
-        Log.w("Launcher", err)
-        Toast.makeText(this, err, Toast.LENGTH_LONG).show()
-        return dir.name
-    }
-
-    private fun launchGame(entry: Entry) {
-        if (entry.hasError) {
+    private fun launchGame(item: Item) {
+        if (item.error != null) {
             AlertDialog.Builder(this)
                 .setTitle(R.string.error)
-                .setMessage(getString(R.string.toast_no_ini, entry.path))
+                .setMessage(item.error)
                 .setPositiveButton("OK", null)
                 .show()
             return
         }
         val i = Intent()
         i.setClass(applicationContext, XSystem4Activity::class.java)
-        i.putExtra(XSystem4Activity.EXTRA_GAME_ROOT, entry.path.path)
-        i.putExtra(XSystem4Activity.EXTRA_XSYSTEM4_HOME, entry.homedir.path)
+        i.putExtra(XSystem4Activity.EXTRA_GAME_ROOT, item.path.path)
+        i.putExtra(XSystem4Activity.EXTRA_XSYSTEM4_HOME, item.homedir.path)
         startActivity(i)
     }
 }
